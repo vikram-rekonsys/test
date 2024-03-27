9b757462fc2f7e4470e619056621520d04090dd5
vikram <vikram@rekonsys.tech>
2024-03-27 16:46:12 +0530

    added login view for dealer

--- users/forms.py
+++ users/forms.py
@@ -37,6 +37,22 @@ class UserLoginForm(forms.Form):
 
         return cleaned_data
 
+class DealerLoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user or not user.is_active:
+                raise forms.ValidationError("Invalid username or password.")
+
+        return cleaned_data
+
 class UserSignupForm(UserCreationForm):
     class Meta:
         model = User
